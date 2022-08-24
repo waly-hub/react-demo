20dc427711a7eaadfb5b44f30a98ecfeb46e193b
waly <286645611@qq.com>
2022-08-24 23:36:20 +0800

    fix：修改文章图表

--- src/pages/Layout/Article/index.js
+++ src/pages/Layout/Article/index.js
@@ -16,7 +16,7 @@ import { observer } from 'mobx-react-lite'
 import moment from 'moment'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import img404 from '@/assets/error.png'
-
+import uuid from 'react-uuid'
 const { Option } = Select
 const { RangePicker } = DatePicker
 const formItemLayout = {
@@ -54,27 +54,14 @@ const typeList = [
     id: 3
   },
 ]
-const dataSource = [
-  {
-    key: '1',
-    name: '胡彦斌',
-    age: 32,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-]
-
 
 const App = () => {
   const { articleStore } = useSotre()
+
   useEffect(() => {
     articleStore.getChannelList()
   }, [articleStore])
+
   const [params, setParams] = useState({
     page: 1,
     per_page: 10
@@ -82,11 +69,20 @@ const App = () => {
   useEffect(() => {
     articleStore.getArticleDataList(params)
   }, [params])
+
   const onFinish = (values) => {
     console.log('Received values of form: ', values)
   }
   const handleTypeChange = (value) => {
     console.log('handleTypeChange', value)
+  }
+
+  const goPublish = (data) => {
+    console.log('---', data)
+  }
+
+  const delArticle = (data) => {
+    console.log('---', data)
   }
 
   const columns = [
@@ -124,28 +120,28 @@ const App = () => {
       title: '点赞数',
       dataIndex: 'like_count'
     },
-    // {
-    //   title: '操作',
-    //   render: data => {
-    //     return (
-    //       <Space size="middle">
-    //         <Button
-    //           type="primary"
-    //           shape="circle"
-    //           icon={<EditOutlined />}
-    //           onClick={() => goPublish(data)} />
-    //         <Button
-    //           type="primary"
-    //           danger
-    //           shape="circle"
-    //           icon={<DeleteOutlined />}
-    //           onClick={() => delArticle(data)}
-    //         />
-    //       </Space>
-    //     )
-    //   },
-    //   fixed: 'right'
-    // }
+    {
+      title: '操作',
+      render: data => {
+        return (
+          <Space size="middle">
+            <Button
+              type="primary"
+              shape="circle"
+              icon={<EditOutlined />}
+              onClick={() => goPublish(data)} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+              onClick={() => delArticle(data)}
+            />
+          </Space>
+        )
+      },
+      fixed: 'right'
+    }
   ]
 
 
@@ -165,7 +161,7 @@ const App = () => {
             <Form.Item label="状态" name='status'>
               <Select onChange={handleTypeChange}>
                 {typeList.map(item => (
-                  <Option key={item.id} value={item.value}>
+                  <Option key={uuid()} value={item.value}>
                     {item.name}
                   </Option>
                 ))}
@@ -176,7 +172,7 @@ const App = () => {
             <Form.Item label="频道" name="channel">
               <Select onChange={handleTypeChange}>
                 {articleStore.channelList.map(item => (
-                  <Option key={item.id} value={item.id}>
+                  <Option key={uuid()} value={item.id}>
                     {item.name}
                   </Option>
                 ))}
@@ -195,7 +191,7 @@ const App = () => {
           </Button>
         </Form.Item>
       </Form>
-      <Table dataSource={articleStore.articleList} columns={columns} bordered />
+      <Table dataSource={articleStore.articleList} columns={columns} bordered rowKey={item => item.id} />
     </>
 
   )
